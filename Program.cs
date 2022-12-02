2e0a9e4a823ea861a4875bb27dab70aa0789d585
Xasan <iamxasann@gmail.com>
2022-12-02 17:38:15 +0500

    finished fibonachi and students scores

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿/* Bir xil tipdagi elementlarning to`plamiga massiv
+deyiladi .Massivlarni e`lon(declare) qilishning bir necha 
+yo`li bor. Bularning ichida eng osoni 
+
+1)     string[] cars;
+
+usulida e`lon qilishdir. Bus usulda massiv nechta
+elementdan tashkil topgani va massivning elementlari oldindan
+kiritilmaydi. Faqatgina massiv elementlarining tipi kiritilib 
+o`tiladi. 
+
+2) string[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+bu usulda biz ichida elementlari mavjud bo`lgan massiv yaratamiz.
+Bunda massiv nomidan so`ng " = " belgisini qo`yib " { }"
+belgisining ichiga vergullar bilan ajratib yozib , kiritib o`tamiz.
+
+*/
